56a9f376ce427bf3aff3edc8ce1f359357a8f58a
Nina2498 <shene1998m@gmail.com>
2022-08-11 23:27:23 +0300

    creating http.get

--- lib/src/features/services/kurdish_names_services.dart
+++ lib/src/features/services/kurdish_names_services.dart
@@ -1,7 +1,19 @@
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 class KurdishNamesServices{
   //https://github.com/DevelopersTree/nawikurdi
   fetchListOfNames(){
+    //TODO: create the URI
+    //scheme: https,host: nawikurdi.com, path:api,
+    Uri _KurdishNamesUri = Uri(
+       scheme: 'https',
+       host: 'nawikurdi.com', 
+       path: 'api', 
+       queryParameters:{
+      'limit': '5',
+      'gender': "F",
+      'offset': '0',
 
+  });
+    http.get(_KurdishNamesUri);
   }
 }
